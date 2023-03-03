2813b54ffaac873be4eb670ba9561acc7c62d6dd
Ratnesh Sharma <ratnesh.at.24@gmail.com>
2023-03-03 09:50:05 +0530

    tailwind css integrated

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Layout from '@/components/Layout'
+import Layout from '../components/Layout';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='dark-mode'>
+      <body>
         <Layout>
           <Main />
         </Layout>
